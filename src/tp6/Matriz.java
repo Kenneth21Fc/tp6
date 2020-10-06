1d0ed954413c159f3db4a8f432849aed68ad8227
kenneth <roxi@roxi-PC>
2020-10-05 21:52:41 -0300

    tp6

---
+++ src/tp6/Matriz.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp6;
+
+/**
+ *
+ * @author roxi
+ */
+public class Matriz {
+    
+    public static void armar(){
+        int matriz[] []= new int [4] [];
+        matriz[0] = new int [4];
+        matriz[1] = new int [3];
+        matriz[2] = new int [5];
+        matriz[3] = new int [2];
+        int i = 3; 
+        for (int f = 0; f < matriz.length; f++){
+            for( int c = 0; c < matriz[f].length; c++){
+                matriz [f] [c] = i;
+                i += 3;
+            }
+        }
+        System.out.println("\n");
+        for (int ft = 0; ft < matriz.length; ft++){
+            System.out.println("Fila "+ ft + " los numeros pares son los siguientes: ");
+            for( int ca = 0; ca < matriz[ft].length; ca++){
+                System.out.println("Es par " + matriz[ft] [ca] );
+            }
+        }
+    }
+    public static void main (String[] args){
+        Matriz.armar();
+    }
+    
+}
+
